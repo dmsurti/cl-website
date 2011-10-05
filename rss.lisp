83c94338ca87dbc539d3e30f0cf50fbeb55505ee
Deepak Manharlal Surti <dmsurti@gmail.com>
2011-10-06 04:05:47 +0530

    Fix for rss date and rss xml not generated correctly

--- rss.lisp
+++ rss.lisp
@@ -6,26 +6,43 @@
   "Generatest the rss link for content in tex."
   (concat "http://deepaksurti.com/" (file-wext tex "html")))
 
-(defmacro generate-rss ()
+(defparameter *days*
+    '("Monday" "Tuesday" "Wednesday"
+      "Thursday" "Friday" "Saturday"
+      "Sunday"))
+
+(defun rss-date (dir tex)
+  (let ((parsed (parse-date dir tex)))
+    (multiple-value-bind (y m d) (file-ymd dir tex)
+      (multiple-value-bind (i g n o r e dow ign ore) 
+			   (decode-universal-time parsed)
+	(concat (nth dow *days*) ", "
+		d " "
+		m " " 
+		y " " 
+	       "00:00:01" " "
+	       "IST")))))
+		
+(defun generate-rss ()
   "Generates the rss xml feed."
-  (let ((file (concat *sitehtmldir* "rss.xml")))
-    `(with-open-file (str ,file :direction :output
-			  :if-exists :supersede :if-does-not-exist :create)
+  (let ((file (concat *sitehtmldir* "rss.xml"))
+	(files (files-by-date)))
+    (with-open-file (str file :direction :output
+		              :if-exists :supersede 
+			      :if-does-not-exist :create)
        (xml-emitter:with-rss2 (str :encoding "utf-8")
-	 (xml-emitter:rss-channel-header *sitename* 
+	 (xml-emitter:rss-channel-header "miracle" 
 					 "http://deepaksurti.com"
-					 :description "About my work in Lisp, Java, Flex and more...")
-	 ,@(apply #'append
-		  (mapcar #'(lambda (p)
-			      (let* ((dir (nth 3 p))
-				     (file (nth 4 p))
-				     (title (file-meta dir file "\title{"))
-				     (link (rss-link dir file))
-				     (date (file-meta dir file "\date{")))
-				(xml-emitter:rss-item title
-						      :author (concat *author* " " *email*)
-						      :link link
-						      :description title
-						      :guid link
-						      :pubDate date))) 
-			  (files-by-date)))))))
+					 :description "to see a miracle, be the miracle")
+	 (dolist (p files)
+	   (let* ((dir (nth 3 p))
+		  (file (nth 4 p))
+		  (title (file-meta dir file "\title{"))
+		  (link (rss-link dir file))
+		  (date (rss-date dir file)))
+	     (xml-emitter:rss-item title
+				   :author (concat *author* " " *email*)
+				   :link link
+				   :description title
+				   :guid link
+				   :pubDate date)))))))
